1513d86c1c3855039083f6929bf1c5bb2b39d1e6
Michael B <michaelb@Michaels-MacBook-Pro-2.local>
2021-11-02 23:30:48 -0400

    intial commit

---
+++ src/main/java/com/gm/takeHomeService/TakeHomeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gm.takeHomeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHomeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gm/takeHomeService/TakeHomeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.takeHomeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
